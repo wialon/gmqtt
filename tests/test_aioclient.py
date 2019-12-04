da05359c4e8a45d0869dc0e825a116ea26b531c8
Liam Diprose <liam.diprose@pbanz.com>
2019-12-04 17:40:28 +1300

    Add tests for expect-duplicated messges

--- tests/test_aioclient.py
+++ tests/test_aioclient.py
@@ -1,8 +1,11 @@
 import pytest
+from unittest.mock import MagicMock
 
 import gmqtt
 from gmqtt import aioclient, message_queue
 from gmqtt.message_queue import TopicFilter
+from gmqtt.aioclient import MqttClientWrapper
+from gmqtt.client import Message
 
 # TODO: Fixtures
 # Mock client
@@ -57,3 +60,32 @@ def test_invalid_multilevel_wildcard():
 
     with pytest.raises(ValueError):
         TopicFilter("sport/tennis#")
+
+
+@pytest.fixture
+def client():
+    mock_inner = MagicMock()
+    mock_inner.subscribe.return_value = None
+    mock_inner.on_message = None
+
+    wrapper_client = MqttClientWrapper(mock_inner)
+
+    return wrapper_client, mock_inner
+
+@pytest.mark.asyncio
+async def test_multiple_subs_duplicate_messages(client):
+    client, mocked_inner = client
+
+    sub1 = await client.subscribe("test/test")
+    sub2 = await client.subscribe("test/test")
+
+    message = Message(topic="test/test", payload="payload")
+    mocked_inner.on_message(client=mocked_inner, topic=message.topic, payload=message.payload, qos=message.qos, properties={})
+
+    msg1 = await sub1.recv()
+    msg2 = await sub2.recv()
+
+    assert msg1.topic == "test/test"
+    assert msg2.topic == "test/test"
+    assert msg1.payload == b"payload"
+    assert msg2.payload == b"payload"
