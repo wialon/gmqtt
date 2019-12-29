adeae3b7fd82f6011f350e30efdfef34d0e16fc7
Fabian Affolter <mail@fabian-affolter.ch>
2019-12-29 23:47:09 +0100

    Soften the requirements
    Strict pinning block the installation outside of a `venv`. E.g., Fedora already ships `pytest-asyncio-0.10.0`.

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ setup(name='gmqtt',
       zip_safe=True,
       install_requires=[],
       tests_requires=[
-          'pytest==3.8.0',
-          'pytest-cov==2.6.0',
-          'pytest-asyncio==0.9.0',
+          'pytest>=3.8.0',
+          'pytest-cov>=2.6.0',
+          'pytest-asyncio>=0.9.0',
       ]
       )
