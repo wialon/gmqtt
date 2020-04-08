c622160cbf21842689caa5adcdb541878ddb2fbb
Lenka42 <elena.shylko@gmail.com>
2020-04-08 16:21:19 +0300

    Enhancements on keepalive functionality (#105)
    * Enhancements on keepalive functionality
    
    * Fix case when no messages from broker for a long time
    
    * Up version

--- gmqtt/mqtt/connection.py
+++ gmqtt/mqtt/connection.py
@@ -28,15 +28,17 @@ class MQTTConnection(object):
         return MQTTConnection(transport, protocol, clean_session, keepalive)
 
     def _keep_connection(self):
-        if self.is_closing():
+        if self.is_closing() or not self._keepalive:
             return
 
-        if time.monotonic() - self._last_data_in >= 2 * self._keepalive:
+        time_ = time.monotonic()
+        if time_ - self._last_data_in >= 2 * self._keepalive:
             logger.warning("[LOST HEARTBEAT FOR %s SECONDS, GOING TO CLOSE CONNECTION]", 2 * self._keepalive)
             asyncio.ensure_future(self.close())
             return
 
-        if time.monotonic() - self._last_data_in >= 0.8 * self._keepalive:
+        if time_ - self._last_data_out >= 0.8 * self._keepalive or \
+                time_ - self._last_data_in >= 0.8 * self._keepalive:
             self._send_ping_request()
         self._keep_connection_callback = asyncio.get_event_loop().call_later(self._keepalive / 2, self._keep_connection)
 
@@ -47,6 +49,7 @@ class MQTTConnection(object):
     def send_package(self, package):
         # This is not blocking operation, because transport place the data
         # to the buffer, and this buffer flushing async
+        self._last_data_out = time.monotonic()
         if isinstance(package, (bytes, bytearray)):
             package = package
         else:
@@ -83,8 +86,22 @@ class MQTTConnection(object):
         self._handler = handler
 
     async def close(self):
-        self._keep_connection_callback.cancel()
+        if self._keep_connection_callback:
+            self._keep_connection_callback.cancel()
         self._transport.close()
 
     def is_closing(self):
         return self._transport.is_closing()
+
+    @property
+    def keepalive(self):
+        return self._keepalive
+
+    @keepalive.setter
+    def keepalive(self, value):
+        if self._keepalive == value:
+            return
+        self._keepalive = value
+        if self._keep_connection_callback:
+            self._keep_connection_callback.cancel()
+        self._keep_connection_callback = asyncio.get_event_loop().call_later(self._keepalive / 2, self._keep_connection)
