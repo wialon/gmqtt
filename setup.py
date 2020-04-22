70862c596b90402db0c352073bdc34e37d883141
Elena Shylko <elena.shylko@gmail.com>
2020-04-22 17:05:16 +0300

    Temporarily fix pytest-asyncio version to 0.10.0

--- setup.py
+++ setup.py
@@ -35,9 +35,9 @@ TESTS_REQUIRE = [
     "more-itertools>=7.0.0",
     "pluggy>=0.11.0",
     "py>=1.8.0",
-    "pytest-asyncio>=0.10.0",
+    "pytest-asyncio==0.10.0",
     "pytest-cov>=2.7.1",
-    "pytest>=4.5.0",
+    "pytest>=5.4.0",
     "six>=1.12.0",
     "uvloop>=0.14.0",
 ]
